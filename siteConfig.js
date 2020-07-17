c2ad33de13fe2bbe7da1a1b86bc1c292fc093a09
Banmua <banmua72@gmail.com>
2020-07-16 20:05:34 -0700

    merge

--- siteConfig.js
+++ siteConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Do not include a trailing slash!
-    siteUrl: `https://demo.jamify.org`,
+    siteUrl: `https://nghitruong.com`,
 
     // Enable infinite scroll (default: true)
     infiniteScroll: true,
